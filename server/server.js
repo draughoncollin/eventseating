4f1252d0ed4f49b0a25dec2a6292d65c0bb33259
Collin Draughon <draughoncollin@gmail.com>
2021-08-03 12:27:33 -0500

    removed session storage

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ Shopify.Context.initialize({
   API_VERSION: ApiVersion.October20,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
-  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+  //SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
 });
 
 // Storing the currently active shops in memory will force them to re-login when your server restarts. You should
